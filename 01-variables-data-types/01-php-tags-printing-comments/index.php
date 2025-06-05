e86a4703487f1ac2f88f5088cc13c1fa09f358e7
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-05 18:37:09 +0100

    Ajout de balises PHP pour l'affichage de texte dynamique dans le fichier index.php

--- 01-variables-data-types/01-php-tags-printing-comments/index.php
+++ 01-variables-data-types/01-php-tags-printing-comments/index.php
@@ -1,3 +1,7 @@
+<?php
+echo 'Hello From PHP';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,13 +15,13 @@
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold"><?php echo 'Learn PHP From Scratch'; ?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
-            <p>In this course, you will learn the fundamentals of the PHP language</p>
+            <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome To The Course' ?></h2>
+            <?php echo '<p>In this course, you will learn the fundamentals of the PHP language</p>' ?>
         </div>
     </div>
 </body>
