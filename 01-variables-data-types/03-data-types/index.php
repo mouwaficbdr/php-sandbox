c2c96b102c2db200c14441044af4d59696c88a05
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-05 18:51:07 +0100

    Ajout de l'affichage des types de données PHP dans index.php

--- 01-variables-data-types/03-data-types/index.php
+++ 01-variables-data-types/03-data-types/index.php
@@ -12,3 +12,20 @@
 - NULL
 - Resource
 */
+
+//Strings
+$name = 'Mouwafic Badarou';
+$name2 = 'John Doe';
+var_dump($name);
+echo '<br>';
+
+//Integers
+$age = 35;
+var_dump($age);
+echo '<br>';
+
+//Arrays
+$friends = ['John', 'Jack', 'Jane'];
+var_dump($friends);
+
+//Ressource
