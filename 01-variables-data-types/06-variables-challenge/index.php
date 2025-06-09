0e519c78302f8afbdc12f1c8d636551db1336253
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-09 08:16:19 +0100

    Refactor l'affichage dynamique des titres et du contenu dans index.php

--- 01-variables-data-types/06-variables-challenge/index.php
+++ 01-variables-data-types/06-variables-challenge/index.php
@@ -1,19 +1,26 @@
+<?php
+  $pageTitle = 'Brad\'s PHP Blog';
+  $title = 'Introduction to PHP';
+  $author = 'By: John Doe';
+  $body = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting
+        language that has revolutionized web development. With its simplicity,
+        flexibility, and vast community support, PHP has become the backbone of
+        countless dynamic websites and web applications.';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Brad's PHP Blog | Introduction to PHP</title>
+    <title><?= "$pageTitle | $title" ?></title>
   </head>
   <body>
     <main>
-      <h1>Introduction to PHP</h1>
-      <p>By: John Doe</p>
+      <h1><?= $title ?></h1>
+      <p><?= $author ?></p>
       <p>
-        PHP (Hypertext Preprocessor) is a widely used server-side scripting
-        language that has revolutionized web development. With its simplicity,
-        flexibility, and vast community support, PHP has become the backbone of
-        countless dynamic websites and web applications.
+        <?= $body ?>
       </p>
     </main>
   </body>
