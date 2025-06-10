f745c2e247d5acf11de0476555518014034bbb8c
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-10 18:04:15 +0100

    Ajout de la classe Admin qui étend la classe User avec un niveau d'accès

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,15 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+
+class Admin extends User {
+  public $level;
+
+  public function __construct($name, $email, $level) {
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+}
+
+$admin = new Admin('Tom', 'gmail.com', 5);
